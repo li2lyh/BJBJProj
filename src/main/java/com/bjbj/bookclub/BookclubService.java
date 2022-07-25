c4c6beebdac07ef16439da6d7e7977324e2dbcb2
YeJiDev <hanyj1994@gmail.com>
2022-07-25 17:50:02 +0900

    bookclub modify together

--- src/main/java/com/bjbj/bookclub/BookclubService.java
+++ src/main/java/com/bjbj/bookclub/BookclubService.java
@@ -2,9 +2,6 @@ package com.bjbj.bookclub;
 
 
 import java.util.List;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bjbj.member.MemberDAO;
+import com.bjbj.utils.PageDTO;
 
 @Service
 public class BookclubService {
@@ -20,69 +18,69 @@ public class BookclubService {
 	@Autowired
 	private MemberDAO memberDao;
   
-  /* 전제 조회 */
+  /* �쟾�젣 議고쉶 */
 	public List<BookclubDTO> selectAll() throws Exception{
 		return dao.selectAll();
 	}
 	
-	/* 최근순으로 조회 */
+	/* 理쒓렐�닚�쑝濡� 議고쉶 */
 	public List<BookclubDTO> selectLately() throws Exception {
 		return dao.selectLately();
 	}
 	
-	/* 페이징 */
+	/* �럹�씠吏� */
 	public void getPage(HttpServletRequest request) throws Exception{
-      //한 페이지에 몇개씩 표시할 것인지
+      //�븳 �럹�씠吏��뿉 紐뉕컻�뵫 �몴�떆�븷 寃껋씤吏�
       final int PAGE_ROW_COUNT=5;
-      //하단 페이지를 몇개씩 표시할 것인지
+      //�븯�떒 �럹�씠吏�瑜� 紐뉕컻�뵫 �몴�떆�븷 寃껋씤吏�
       final int PAGE_DISPLAY_COUNT=5;
       
-      //보여줄 페이지의 번호를 일단 1이라고 초기값 지정
+      //蹂댁뿬以� �럹�씠吏��쓽 踰덊샇瑜� �씪�떒 1�씠�씪怨� 珥덇린媛� 吏��젙
       int pageNum=1;
-      //페이지 번호가 파라미터로 전달되는지 읽어와 본다.
+      //�럹�씠吏� 踰덊샇媛� �뙆�씪誘명꽣濡� �쟾�떖�릺�뒗吏� �씫�뼱�� 蹂몃떎.
       String strPageNum = request.getParameter("pageNum");
-      //만일 페이지 번호가 파라미터로 넘어 온다면
+      //留뚯씪 �럹�씠吏� 踰덊샇媛� �뙆�씪誘명꽣濡� �꽆�뼱 �삩�떎硫�
       if(strPageNum != null){
-         //숫자로 바꿔서 보여줄 페이지 번호로 지정한다.
+         //�닽�옄濡� 諛붽퓭�꽌 蹂댁뿬以� �럹�씠吏� 踰덊샇濡� 吏��젙�븳�떎.
          pageNum=Integer.parseInt(strPageNum);
       }
       
-      //보여줄 페이지의 시작 ROWNUM
+      //蹂댁뿬以� �럹�씠吏��쓽 �떆�옉 ROWNUM
       int startRowNum = 1 + (pageNum-1) * PAGE_ROW_COUNT;
-      //보여줄 페이지의 끝 ROWNUM
+      //蹂댁뿬以� �럹�씠吏��쓽 �걹 ROWNUM
       int endRowNum = pageNum * PAGE_ROW_COUNT;
       
-      //startRowNum 과 endRowNum  을 BookclubDTO 객체에 담고
-      BookclubDTO dto = new BookclubDTO();
+      //startRowNum 怨� endRowNum  �쓣 BookclubDTO 媛앹껜�뿉 �떞怨�
+      PageDTO dto = new PageDTO();
       dto.setStartRowNum(startRowNum);
       dto.setEndRowNum(endRowNum);
       
-      // BookclubDTO 객체를 이용해서 목록을 얻어온다.
+      // BookclubDTO 媛앹껜瑜� �씠�슜�빐�꽌 紐⑸줉�쓣 �뼸�뼱�삩�떎.
       List<BookclubDTO> list = dao.selectPage(startRowNum, endRowNum);
       
-      //하단 시작 페이지 번호 
+      //�븯�떒 �떆�옉 �럹�씠吏� 踰덊샇 
       int startPageNum = 1 + ((pageNum-1)/PAGE_DISPLAY_COUNT) * PAGE_DISPLAY_COUNT;
-      //하단 끝 페이지 번호
+      //�븯�떒 �걹 �럹�씠吏� 踰덊샇
       int endPageNum = startPageNum + PAGE_DISPLAY_COUNT - 1;
       
-      //전체 row 의 갯수
+      //�쟾泥� row �쓽 媛��닔
       int totalRow = dao.getCount();
-      //전체 페이지의 갯수 구하기
+      //�쟾泥� �럹�씠吏��쓽 媛��닔 援ы븯湲�
       int totalPageCount = (int)Math.ceil(totalRow / (double)PAGE_ROW_COUNT);
-      //끝 페이지 번호가 이미 전체 페이지 갯수보다 크게 계산되었다면 잘못된 값이다.
+      //�걹 �럹�씠吏� 踰덊샇媛� �씠誘� �쟾泥� �럹�씠吏� 媛��닔蹂대떎 �겕寃� 怨꾩궛�릺�뿀�떎硫� �옒紐삳맂 媛믪씠�떎.
       if(endPageNum > totalPageCount){
-         endPageNum = totalPageCount; //보정해 준다. 
+         endPageNum = totalPageCount; //蹂댁젙�빐 以��떎. 
       }
       
-      //request 영역에 담아주기
+      //request �쁺�뿭�뿉 �떞�븘二쇨린
       request.setAttribute("list", list);   //Bookclub list
-      request.setAttribute("startPageNum", startPageNum);   //시작 페이지 번호
-      request.setAttribute("endPageNum", endPageNum);   //끝 페이지 번호
-      request.setAttribute("pageNum", pageNum);   //현재 페이지 번호
-      request.setAttribute("totalPageCount", totalPageCount);   //모든 페이지 count	      
+      request.setAttribute("startPageNum", startPageNum);   //�떆�옉 �럹�씠吏� 踰덊샇
+      request.setAttribute("endPageNum", endPageNum);   //�걹 �럹�씠吏� 踰덊샇
+      request.setAttribute("pageNum", pageNum);   //�쁽�옱 �럹�씠吏� 踰덊샇
+      request.setAttribute("totalPageCount", totalPageCount);   //紐⑤뱺 �럹�씠吏� count	      
 	}
 	
-	/* 찜한 모임 조회 */
+	/* 李쒗븳 紐⑥엫 議고쉶 */
 	public List<BookclubDTO> likeClub() throws Exception {
 		return dao.likeClub();
 	}
@@ -93,7 +91,7 @@ public class BookclubService {
 	}
 
 	public void insert(BookclubDTO dto, RoleDTO roleDto) throws Exception {
-		// room_id 시퀀스 미리 생성 (role table, bookroom table 동시 적용)
+		// room_id �떆���뒪 誘몃━ �깮�꽦 (role table, bookroom table �룞�떆 �쟻�슜)
 		int room_id = dao.selectSeq();
 		dto.setRoom_id(room_id);
 		roleDto.setRoom_id(room_id);
@@ -106,47 +104,47 @@ public class BookclubService {
 		dao.insertRole(dto);
 	}
 
-	// room_id 에 따른 한개 리스트 가져오기
+	// room_id �뿉 �뵲瑜� �븳媛� 由ъ뒪�듃 媛��졇�삤湲�
 	public BookclubDTO selectOne(int room_id) throws Exception {
 		return dao.selectOne(room_id);
 	}
 
-	// email로 해당 role 데이터 가져오기
+	// email濡� �빐�떦 role �뜲�씠�꽣 媛��졇�삤湲�
 	public RoleDTO selectRole(String email) throws Exception {
 		return dao.selectRole(email);
 	}
 
-	// 날짜표기 format (String to String)
+	// �궇吏쒗몴湲� format (String to String)
 	public String getStrDate(String string) throws Exception {
 		return dao.getStrDate(string);
 	}
 
-	// 자기소개 데이터 업데이트
+	// �옄湲곗냼媛� �뜲�씠�꽣 �뾽�뜲�씠�듃
 	public int updateMydesc(String email, String mydesc) throws Exception {
 		return memberDao.updateMydesc(email, mydesc);
 	}
 
-	// waiting 테이블 데이터 삽입
+	// waiting �뀒�씠釉� �뜲�씠�꽣 �궫�엯
 	public void insertWaiting(WaitingDTO dto) throws Exception {
 		dao.insertWaiting(dto);
 	}
 
-	// 이메일로 waiting 테이블 조회
+	// �씠硫붿씪濡� waiting �뀒�씠釉� 議고쉶
 	public WaitingDTO selectByEmail(String email) throws Exception {
 		return dao.selectByEmail(email);
 	}
 
-	// Room_id 에 따른 지원 인원 보기
+	// Room_id �뿉 �뵲瑜� 吏��썝 �씤�썝 蹂닿린
 	public List<String> selectByRoom(int room_id) throws Exception {
 		return dao.selectByRoom(room_id);
 	}
 
-	// waiting 테이블에서 이메일로 해당 데이터 삭제
+	// waiting �뀒�씠釉붿뿉�꽌 �씠硫붿씪濡� �빐�떦 �뜲�씠�꽣 �궘�젣
 	public void deleteByEmail(String email) throws Exception {
 		dao.deleteByEmail(email);
 	}
 
-	// 현재인원 + 1
+	// �쁽�옱�씤�썝 + 1
 	public void updateCurrent(int room_id) throws Exception {
 		dao.updateCurrent(room_id);
 	}
