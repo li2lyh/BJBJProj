dcdb6967891012a371fed98a957647f4e5deae79
YEON HYEWON <debora3744@naver.com>
2022-07-29 09:43:01 +0900

    220729 review

--- src/main/java/com/bjbj/review/ReviewController.java
+++ src/main/java/com/bjbj/review/ReviewController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+@RequestMapping(value = "/review")
 @Controller
 public class ReviewController {
 	// 리뷰 게시판과 관련된 요청 처리
